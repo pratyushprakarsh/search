07d98a3572f8847dc95733f9fd8c1321d1573f98
Pratyush <pratyush.prakarsh@gmail.com>
2020-05-24 20:13:13 +0530

    initial commit

---
+++ src/test/java/com/pratyush/search/SearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratyush.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
